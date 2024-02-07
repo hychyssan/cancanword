286f24190b48c9d6087453b64cb26dda7c8b1443
urmbrella <huyicesan@gmail.com>
2024-02-07 22:52:29 +0800

    ✨ feat(nand2tetris\projects\04):  完成project04

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+ @i
+ M=0
+
+ (PRESS)
+ //if unpress ,jump to@TURNTOUNPRESS
+ @KBD
+ D=M
+ @TURNTOUNPRESS
+ D;JEQ
+
+ //if(i==8000,i=0)
+ @i
+ D=M
+ @8000
+ D=A-D
+ @TURNTOPRESS
+ D;JEQ
+
+
+ //RAM[SCREEN+i]=-1
+ @SCREEN
+ D=A
+ @i
+ A=M+D
+ M=-1
+
+ //i++
+ @i
+ M=M+1
+
+ @PRESS
+ 0;JMP
+
+ (UNPRESS)
+  //if press ,jump to@TURNTOPRESS
+ @KBD
+ D=M
+ @TURNTOPRESS
+ D;JNE
+
+ //if i==8000,goto @UNPRESS
+ //if(i==8000,i=0)
+ @i
+ D=M
+ @8000
+ D=A-D
+ @TURNTOUNPRESS
+ D;JEQ
+
+ //RAM[SCREEN+i]=0
+ @SCREEN
+ D=A
+ @i
+ A=M+D
+ M=0
+
+ //i++
+ @i
+ M=M+1
+
+ @UNPRESS
+ 0;JMP
+
+ (TURNTOUNPRESS)
+ @i
+ M=0
+ @UNPRESS
+ 0;JMP
+
+ (TURNTOPRESS)
+ @i
+ M=0
+ @PRESS
+ 0;JMP
+ 
